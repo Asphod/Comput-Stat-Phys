85df2d5a84ffb6beee5e3f70c1c44f1d13d4d4f4
Asphod <francois.gu@ens-lyon.fr>
2019-10-17 13:45:33 +0200

    Avancé 16/10

--- IsingMC_done.py
+++ IsingMC_done.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 from matplotlib import gridspec
 import numbers
 import math
+import matplotlib as mpl
 from matplotlib import animation, rc
-
 
 
 def Conf2D(LSpin, q=2, type0='up'):
     """
-    Returns a 2d integer array of s=+/-1 vaules representing a configuration of 
+    Returns a 2d integer array of s=+/-1 vaules representing a configuration of
     an (LSpin x LSpin) Ising system.
     Allowed types:
         "up":           all-spins-up cofiguration
@@ -22,21 +22,34 @@ def Conf2D(LSpin, q=2, type0='up'):
     """
     if type0 == 'up':
         conf = np.zeros( (LSpin,LSpin) , dtype=int)
-#    elif type0 == 'down':
-#        conf = -np.ones( (LSpin,LSpin) , dtype=int)
+    elif type0 == 'alternating' or type0 == 'checkerboard':
+        if q%2 == 0:
+            conf = np.zeros( (LSpin,LSpin) , dtype=int)
+            conf[1::2,0::2] = q//2 # in even lines set odd elemets
+            conf[0::2,1::2] = q//2 # in odd lines set even elements
+        else:
+            print('No checkerboard with odd q yet. Returning up')
+            conf = np.zeros( (LSpin,LSpin) , dtype=int)
+    elif type0 == 'down':
+            if q%2 == 0:
+                conf = q//2*np.ones( (LSpin,LSpin) , dtype=int)
+            else:
+                print('No checkerboard with odd q yet. Returning up')
+                conf = np.zeros( (LSpin,LSpin) , dtype=int)
 #    elif type0 == 'alternating' or type == 'checkerboard':
 #        conf = np.ones( (LSpin,LSpin) , dtype=int)
 #        conf[1::2,0::2]=-1 # in even lines set odd elemets
 #        conf[0::2,1::2]=-1 # in odd lines set even elements
     elif type0 == 'random':
-        conf=np.random.randint(0,q,(LSpin,LSpin))
+        conf = np.random.randint(0,q,(LSpin,LSpin))
     else:
-        conf = np.zeros( (LSpin,LSpin) , dtype=int)
+        conf = np.zeros( (LSpin,LSpin) , dtype = int)
+
     return conf
 
 
-
-def BlackOrWhiteSweep(h,k,conf,q,even):
+#%%
+def BlackOrWhiteSweep(h,k,conf, q=2, even = True):
     """
     returns conf after executing one odd or even checkerboard spin flip sweep
     """
@@ -49,8 +62,10 @@ def BlackOrWhiteSweep(h,k,conf,q,even):
 
     #sweep dans la conftrial
     if even:
-        conftrial[0::2,0::2]=trial[0::2,0::2]
+        conftrial[1::2,0::2]=trial[1::2,0::2]
+        conftrial[0::2,1::2]=trial[0::2,1::2]
     else:
+        conftrial[0::2,0::2]=trial[0::2,0::2]
         conftrial[1::2,1::2]=trial[1::2,1::2]
 
     #calcul des énergies à t et t+1
@@ -64,21 +79,12 @@ def BlackOrWhiteSweep(h,k,conf,q,even):
     #matrice des probas en fonction de l'énergie
     acc = np.exp(DeltaE)
 
-    #flip ou non
-    flip = np.sign(xi-acc).astype(int)
-    flip[x == -1] = 0
-
-    #construction de la conf après trial
-    trial = trial*flip
-    if even:
-        conf[0::2,0::2]=trial[0::2,0::2]
-    else:
-        conf[1::2,1::2]=trial[1::2,1::2]
-    return conf
+    change = xi - acc
+    conf[change > 0] = conftrial[change > 0]
+    return np.array(conf)
 
 
-
-def CheckerboardSweep(h,k,conf,q):
+def CheckerboardSweep(h,k,conf,q = 2):
     """
     returns conf after executing one odd and one even checkerboard spin flip sweep in random order
     """
@@ -89,28 +95,81 @@ def CheckerboardSweep(h,k,conf,q):
 
     #tirage d'une valeur aléatoire
     if np.random.rand(1)[0]<0.5:
-        newconf = BlackOrWhiteSweep(h,k,conf,q,even=False)
-        newconf = BlackOrWhiteSweep(h,k,newconf,q,even=True)
+        newconf = BlackOrWhiteSweep(h,k,conf,q = q,even=False)
+        newconf = BlackOrWhiteSweep(h,k,newconf,q = q,even=True)
     else:
-        newconf = BlackOrWhiteSweep(h,k,conf,q,even=True)
-        newconf = BlackOrWhiteSweep(h,k,newconf,q,even=False)
+        newconf = BlackOrWhiteSweep(h,k,conf,q = q,even=True)
+        newconf = BlackOrWhiteSweep(h,k,newconf,q = q,even=False)
     return newconf
 
 
+def testrun(h,k,startconf,q=2,NSweeps=10):
+    """
+    returns all the conf for NSweeps
+    """
+    run = [startconf]
+    conf = np.copy(startconf)
+    for i in range(0,NSweeps):
+        conf = CheckerboardSweep(h,k,conf,q=q)
+        run.append(conf)
+    return run
 
-def LocalNNCoupling(conf):
+
+
+def LocalNNCoupling(confrad):
     """ Returns a matrix of the dimension of the configuration matrix with total nearest-neighbor
     coupling for a spin configuration. Conf must be in radians for the scalar product"""
-    Theta_diff_right = np.cos(conf - np.roll(conf, 1, axis = 1))
-    Theta_diff_left =  np.cos(conf - np.roll(conf, -1, axis = 1))
-    Theta_diff_up =  np.cos(conf - np.roll(conf, 1, axis = 0))
-    Theta_diff_down =  np.cos(conf - np.roll(conf, -1, axis = 0))
+    Theta_diff_right = np.cos(confrad - np.roll(confrad, 1, axis = 1))
+    Theta_diff_left =  np.cos(confrad - np.roll(confrad, -1, axis = 1))
+    Theta_diff_up =  np.cos(confrad - np.roll(confrad, 1, axis = 0))
+    Theta_diff_down =  np.cos(confrad - np.roll(confrad, -1, axis = 0))
     LocalNNC = Theta_diff_right + Theta_diff_left + Theta_diff_up + Theta_diff_down
     return LocalNNC
 
 
+def Magnetization(confrad):
+    """
+    Returns the total magnetization for a spin configuration. conf in radians!!
+    """
+    return np.sum(np.cos(confrad))
 
-def ImportanceSampling(h,k,LSpin,NRuns,NSweeps,NInit=0,run_firstconf_list = [],rule='Glauber'):
+def NNCoupling(confrad):
+    """
+    Returns the total nearest-neighbor coupling for a spin configuration. conf in radians!!
+    """
+    return np.sum(LocalNNCoupling(confrad))
+
+
+#%%
+def AnimateMCRun(run,q=2):
+    """
+    Animation of an array of 2D spin conformations. 
+    Down spins are shown blue. Up spins red.
+    """
+    fig = plt.figure()
+    ax = plt.axes()
+    cmap='plasma'
+    norm = mpl.colors.Normalize(vmin=0,vmax=q-1)
+    fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax)
+    im = ax.matshow(run[0], cmap=cmap,norm=norm)
+    ax.set_axis_off()
+ 
+    def init():
+        im.set_data(run[-1])
+ 
+    def animate(i):
+        im.set_array(run[i])
+        return [im]
+
+
+    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=len(run), interval=100)
+    rc('animation', html='html5')
+    return anim
+#%%
+
+
+
+def ImportanceSampling(h,k,LSpin,NRuns,NSweeps,q=2,NInit=0,run_firstconf_list = []):
     """
     Returns four NRunsxNSweeps arrays containing M, Q, m and q for
     LSpin x LSpin Ising conformations generated using Metropolis MC
@@ -133,29 +192,28 @@ def ImportanceSampling(h,k,LSpin,NRuns,NSweeps,NInit=0,run_firstconf_list = [],r
     if len(run_firstconf_list)!=0:
         firstconf_list = run_firstconf_list
 
-################################################################################
-    ##############ATTENTION, PB SUR LES CONFS DE BASE
     else:
         firstconf_list = []
 #        RandomConf = Conf2D(LSpin,'random')
-        UpConf = Conf2D(LSpin,'up')
-        DownConf = Conf2D(LSpin,'down')
-        AltConf = Conf2D(LSpin,'alternating')
+        UpConf = Conf2D(LSpin,q,'up')
+        DownConf = Conf2D(LSpin,q,'down')
+        RandConf = Conf2D(LSpin,q,'random')
+        CheckConf = Conf2D(LSpin,q,'checkerboard')
         for j in np.arange(0,NRuns):
             if j<NRuns/4:
                 firstconf_list.append(UpConf)
-            elif j<NRuns/2:
+            elif j<2*NRuns/4:
                 firstconf_list.append(DownConf)
             elif j<3*NRuns/4:
-                firstconf_list.append(AltConf)
+                firstconf_list.append(RandConf)
             else:
-                firstconf_list.append(-AltConf)
+                firstconf_list.append(CheckConf)
 #                firstconf_list.append(RandomConf)
 #                Random conformations take a very long time to equilibrate
 #                at low temperatures
 ################################################################################
 
-    conf = np.empty((LSpin,LSpin))
+#    conf = np.empty((LSpin,LSpin))
 
     run_M_list = []
     run_Q_list = []
@@ -164,15 +222,15 @@ def ImportanceSampling(h,k,LSpin,NRuns,NSweeps,NInit=0,run_firstconf_list = [],r
         conf = firstconf_list[j]
         # first equilibrate
         for i in np.arange(0,NInit):
-            conf = CheckerboardSweep(h,k,conf,rule)
+            conf = CheckerboardSweep(h,k,conf,q)
 
         # and then start the data acquisition
         M_list = []
         Q_list = []
         for i in np.arange(0,NSweeps):
             conf = CheckerboardSweep(h,k,conf,q)
-            M = Magnetization(conf)
-            Q = NNCoupling(conf)
+            M = Magnetization((2*np.pi/q)*conf)
+            Q = NNCoupling((2*np.pi/q)*conf)
             M_list.append(M)
             Q_list.append(Q)
 
